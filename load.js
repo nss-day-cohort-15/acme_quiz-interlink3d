b9f122d9bdbd1c0ece16f4c3acd102b22eb6b4db
Edgar Barajas <ebarajas05@gmail.com>
2016-08-15 15:20:57 -0500

    working on loadingDOM js page

--- load.js
+++ load.js
@@ -1,1 +1,32 @@
-console.log("Welcome!!")
+var DirtStand = (function (loadInv) {
+  var categories = []
+  var types = []
+  var products = []
+
+
+  loadInv.stockShelves = function (invDisplay) {
+    Promise.all([
+      $.getJSON('categories.json'), 
+      $.getJSON('types.json'),
+      $.getJSON('products')
+      ])
+    
+
+      .then(function(inventory){
+        categories = inventory[0].categories
+        types = inventory[1].types
+        products = inventory[2].products[0]
+        invDisplay(categories,types,products)
+      })
+
+    }
+  return loadInv;
+  console.log("loadInv", DirtStand.categories);
+  console.log("loadInv", DirtStand.types);
+  console.log("loadInv", DirtStand.invDisplay);
+
+})
+
+(DirtStand || {})
+
+
